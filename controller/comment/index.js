ab8e1b6b707fd13a26d6477e83d229eeb48a3d78
tymzyx <httsongli@163.com>
2019-02-01 14:16:25 +0800

    update comment apis

--- controller/comment/index.js
+++ controller/comment/index.js
@@ -1,6 +1,7 @@
 import CommentModel from '../../models/comment/index';
 import uuidv4 from 'uuid/v4';
 import dtime from 'time-formater';
+import BookModel from "../../models/book";
 
 class Comment {
     async addComment(req, res) {
@@ -39,11 +40,29 @@ class Comment {
             console.log('添加评论失败', err);
             res.send({
                 status: 0,
-                type: 'COMMENT_FAILED',
+                type: 'ADD_COMMENT_FAILED',
                 message: '添加评论失败',
             });
         }
 
+    }
+
+    async queryComment(req, res) {
+        try {
+            const { author, toId } = req.query;
+            if (author) {
+                const comment = await BookModel.find({ author, toId });
+            } else {
+                const comment = await BookModel.find({ toId });
+            }
+        } catch (err) {
+            console.log('获取评论失败', err);
+            res.send({
+                status: 0,
+                type: 'GET_COMMENT_FAILED',
+                message: '获取评论失败',
+            });
+        }
     }
 }
 
